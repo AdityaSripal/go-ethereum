60adab379d92573ac1e3bfd3bda8dec4a7b142f6
Aditya Sripal <14364734+AdityaSripal@users.noreply.github.com>
2026-04-23 17:55:42 +0200

    use pebbe raw error string to remove import

--- consensus/aura/aura.go
+++ consensus/aura/aura.go
@@ -25,6 +25,7 @@ import (
 	"io"
 	"math/big"
 	"sort"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -634,7 +635,9 @@ func isEpochEnd(chain consensus.ChainHeaderReader, e *NonTransactionalEpochReade
 	// commit_block -> aura.is_epoch_end
 	for i := range finalized {
 		pendingTransitionProof, err := e.GetPendingEpoch(finalized[i].hash, finalized[i].number)
-		if err != nil {
+		// GNOSIS: pebble returns an error when a non-existent value
+		// isn't found, which is what happens at genesis.
+		if err != nil && !strings.Contains(err.Error(), "pebble: not found") {
 			return nil, err
 		}
 		if pendingTransitionProof == nil {
